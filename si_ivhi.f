66bd427e8e4226e78d31e7124230f9360fe57fd8
Kailash <Ramalingam.Kailasham@monash.edu>
2018-01-19 13:33:35 +1100

    changed path of finconfigs.dat

--- si_ivhi.f
+++ si_ivhi.f
@@ -101,8 +101,9 @@ c
       CHARACTER (LEN=512) CPATH
       PARAMETER(FPATH="/home/rkailasham/Sdrive/Desktop/Kailash_Files/Com
      &bined_Code_Validation/equilibrated_database/eqbconfigs.dat")
-      PARAMETER(CPATH="/home/rkailasham/Sdrive/Desktop/Kailash_Files/fin
-     &configs.dat")
+      PARAMETER(CPATH="finconfigs.dat")
+c      PARAMETER(CPATH="/home/rkailasham/Sdrive/Desktop/Kailash_Files/fin
+c     &configs.dat")
       COMMON /STEPBL/ THI,B,ZMU,RMU2,BAUXQ,BAUXR,DTH,DTQ,SRDT,
      &SRDTH,C1P,C2P,E,AMPL2,BETA,AB,A2,A4,AUX1,AUX2,
      &AUX3,AUX4,AUX5,S,QALPH,GEE1,GEE2,GEE3,GEE4 
